ddb20e58b4c9fd70e2291b40379ef5ed878b0804
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-06-23 01:46:20 +0500

    not found page created

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+function NotFoundPage() {
+    return ( 
+        <div className="flex flex-col items-center justify-center min-h-[70vh] text-center px-4">
+            <h1 className="text-6xl font-extrabold mb-4">404</h1>
+            <h2 className="text-2xl font-bold mb-6">Page Not Found!</h2>
+            <p className="text-l text-muted-foreground mb-8 max-w-md">The page you are looking for does not exist.</p>
+            
+        </div>
+     );
+}
+
+export default NotFoundPage;
