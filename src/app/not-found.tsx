73a63a99233e4dfa36a3310a7b40e6a3b1a505dd
Shahbazali2001 <shahbazali5097855@gmail.com>
2025-06-23 13:26:20 +0500

    Page Not Found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,7 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+
 function NotFoundPage() {
     return ( 
         <div className="flex flex-col items-center justify-center min-h-[70vh] text-center px-4">
@@ -5,6 +9,11 @@ function NotFoundPage() {
             <h2 className="text-2xl font-bold mb-6">Page Not Found!</h2>
             <p className="text-l text-muted-foreground mb-8 max-w-md">The page you are looking for does not exist.</p>
             
+          
+          <Button asChild>
+            <Link href="/">Return to Home</Link>
+          </Button>
+
         </div>
      );
 }
